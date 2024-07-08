b73f4309adc582aa947d3df8cd63ebab88658f3b
LMH <90649822+LoneHilke@users.noreply.github.com>
2024-07-08 20:46:14 +0200

    Prøver en ny deploy
    https: //www.linkedin.com/pulse/deploying-django-website-free-2023-step-by-step-guide-rahman-tfafc/
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- perler/perler/perler/settings.py
+++ perler/perler/perler/settings.py
@@ -39,11 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'plade',
-    'jfrog-pypi-trial',
+    #'jfrog-pypi-trial',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,9 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/image/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
